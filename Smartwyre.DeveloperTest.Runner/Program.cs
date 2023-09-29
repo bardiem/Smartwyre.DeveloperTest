e2a54ec0de2d39eb7fc0316a8825cfcd8dbc5edb
byatsyshyn <80755850+byatsyshyn@users.noreply.github.com>
2023-09-29 17:58:07 +0300

    added console execution

--- Smartwyre.DeveloperTest.Runner/Program.cs
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Types;
+using System;
 
 namespace Smartwyre.DeveloperTest.Runner;
 
@@ -6,6 +9,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        throw new NotImplementedException();
+        var rebateDataStore = new RebateDataStore();
+        var productDataStore = new ProductDataStore();
+
+        var service = new RebateService(rebateDataStore, productDataStore);
+
+        var request = new CalculateRebateRequest { Volume = 1 };
+        Console.WriteLine(service.Calculate(request).Success);
+        //result will be false because DataStore doesn't work and rebase is empty
+        Console.Read();
     }
 }
